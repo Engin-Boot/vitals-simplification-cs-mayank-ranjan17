b48d7fe7c5852d84b0e81d118d33329fea0e1eb4
mayank-ranjan17 <48884605+mayank-ranjan17@users.noreply.github.com>
2020-09-05 17:25:53 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -6,7 +6,6 @@ class Checker
     static int checkvitals=1;
     static void checkbpm(float bpm)
     {
-       //correcting the max range for bpm given as 150 initially
        if(bpm < 70 ) {
              Console.WriteLine("Low beats per minute");
              checkvitals=0;
@@ -24,11 +23,14 @@ class Checker
             Console.WriteLine("Low Spo2 rate");
             checkvitals=0;  
         } 
+       else if(spo2>100){
+           Console.WriteLine("Not a valid range for SpO2");
+           checkvitals=0
+       }
     }
     
     static void checkresprate(float respRate)
     {
-       //respiratory rate is 12 to 16 for a normal adult
        if(respRate < 12 ) {
             Console.WriteLine("Low Respiratory rate");
             checkvitals=0;
@@ -40,8 +42,6 @@ class Checker
         }
         
     }
-    
-    //new feature to check bp
     static void checkbp(float bp)
     {
         if(bp < 80)
@@ -58,7 +58,7 @@ class Checker
     
     static void vitalsAreOk(float bpm, float spo2, float respRate,float bp) {
         
-        checkvitals=1;//condition for all ok case it is being reset to 1 with every call to vitalsAreOk
+        checkvitals=1;
         checkresprate(respRate);
         checkbpm(bpm);
         checkspo2(spo2);
@@ -66,21 +66,7 @@ class Checker
         
         
     }
-    /*static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
-        }
-    }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
-        }
-    }*/
     static int Main() {
-        //ExpectTrue(vitalsAreOk(100, 95, 60));
-        //ExpectFalse(vitalsAreOk(40, 91, 92));
         vitalsAreOk(100,95,60,80);
         if(checkvitals==1)
         {
